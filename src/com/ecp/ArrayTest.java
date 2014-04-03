e24f422b2ce41e9b3569b11b86f77f078373e01c
Administrator <Administrator@OOS6HVCR2DPZ6NR>
2014-04-04 00:02:50 +0700

    Init project

---
+++ src/com/ecp/ArrayTest.java
@@ -0,0 +1,19 @@
+package com.ecp;
+
+public class ArrayTest {
+
+	public static void main(String[] args) {
+		int array [] ;
+        array = new int [5];
+        array [0]=1;
+        array [1]=2;
+        array [2]=3;
+        array [3]=4;
+        array [4]=1;
+	     for(int i = 0; i < array.length; i++)
+	         System.out.println("list[" + i +"] = " + array[i]);
+	     
+	
+      }
+
+}
