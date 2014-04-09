9df652738e89ba217d701ca74e9c4519c5fd5ca5
Administrator <Administrator@172.25.136.158>
2014-04-09 18:02:58 +0700

    Test1

---
+++ Test/src/com/ecp/TestCall.java
@@ -0,0 +1,15 @@
+package com.ecp;
+
+import java.util.Arrays;
+
+public class TestCall {
+	public static void main(String[] args) {
+		BackHam Back = new BackHam();
+		System.out.println("name : "+Back.getName());
+		System.out.println("position : "+Back.getPosition());
+		System.out.println("friend : "+Arrays.toString(Back.getFriend()));
+		Back= new BackHam(1);
+	}
+
+
+}
