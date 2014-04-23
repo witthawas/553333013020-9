1e3d35642985488f23b765da0d08d9835c679316
Administrator <Administrator@172.25.135.182>
2014-04-23 11:17:37 +0700

    improvable

---
+++ src/com/ecp/hello/HelloTest.java
@@ -0,0 +1,12 @@
+package com.ecp.hello;
+
+public class HelloTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Test Github");
+
+	}
+
+}
