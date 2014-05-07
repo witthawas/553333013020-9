123296ad97e56d566018dcc0d357f1b0567ddb93
Administrator <Administrator@172.25.134.75>
2014-05-07 14:52:21 +0700

    improvable

--- src/com/ecp/worksheet_2/TestCall.java
+++ src/com/ecp/worksheet_2/TestCall.java
@@ -3,11 +3,9 @@ package com.ecp.worksheet_2;
 public class TestCall {
 	public static void main(String[] args) {
 		BinaryHeap bhp = new BinaryHeap();
-		bhp.enQueue(13);
-		bhp.enQueue(40);
-		bhp.enQueue(50);
-		bhp.enQueue(60);
-		bhp.enQueue(70);
+		bhp.enQueue(10);
+		bhp.enQueue(20);
+		bhp.enQueue(30);
 		System.out.println(bhp);
       }
 }
